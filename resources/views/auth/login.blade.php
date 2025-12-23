92c4d55892e8cdde30447bf940a9e850214a01af
ravik21 <webartisan.ravi@gmail.com>
2025-12-23 12:25:20 +0530

    Login page updated with backend theme

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,126 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <div class="px-xl-5 px-lg-4 px-3 py-3 page-body my-2 py-2">
+        <div style="height: 90vh;" class="row justify-content-center align-items-center">
+            <div class="col-xl-4 col-lg-5 col-md-7">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-column align-items-center ">
+                            <div class="no-thumbnail rounded-circle border bg-light p-2">
+                                <div class="no-thumbnail rounded-circle border bg-white p-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="34"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-linecap="round" stroke-linejoin="round"
+                                        class=" svg-stroke">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                                        <path d="M16 19h6"></path>
+                                        <path d="M19 16v6"></path>
+                                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="mt-3 ">
+                                <h6 class="text-capitalize text-center fw-bold">Login to your account
+                                </h6>
+                                <span class="text-muted text-capitalize">enter your details to
+                                login</span>
+                            </div>
+                            <div class="mt-3 w-100">
+                                <!-- Session Status -->
+                                <x-auth-session-status class="mb-4" :status="session('status')" />
+                                <form method="POST" class="form-horizontal" id="validateForm" action="{{ route('login') }}">
+                                    @csrf
+                                    <fieldset>
+                                        <!--=== email ===-->
+                                        <div class="mb-3">
+                                            <label for="email" class="form-label">Email
+                                            Address</label>
+                                            <div class="input-group">
+                                                <span class="input-group-text bg-transparent">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                        width="20" viewBox="0 0 24 24"
+                                                        fill="none" stroke="currentColor"
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        class=" svg-stroke">
+                                                        <path stroke="none" d="M0 0h24v24H0z"
+                                                            fill="none"></path>
+                                                        <path
+                                                            d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z">
+                                                        </path>
+                                                        <path d="M3 7l9 6l9 -6"></path>
+                                                    </svg>
+                                                </span>
+                                                <input type="email"
+                                                    class="form-control border-start-0"
+                                                    id="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                                            </div>
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+                                        <!-- Password input-->
+                                        <div class="form-group">
+                                            <label for="password"
+                                                class="form-label">Password</label>
+                                            <div class="input-group">
+                                                <span class="input-group-text bg-transparent">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                        width="20" viewBox="0 0 24 24"
+                                                        fill="none" stroke="currentColor"
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        class=" svg-stroke">
+                                                        <path stroke="none" d="M0 0h24v24H0z"
+                                                            fill="none"></path>
+                                                        <path
+                                                            d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z">
+                                                        </path>
+                                                        <path d="M8 11v-4a4 4 0 1 1 8 0v4">
+                                                        </path>
+                                                        <path d="M15 16h.01"></path>
+                                                        <path d="M12.01 16h.01"></path>
+                                                        <path d="M9.02 16h.01"></path>
+                                                    </svg>
+                                                </span>
+                                                <input type="password"
+                                                    class="form-control border-start-0"
+                                                    id="password" name="password" required autocomplete="current-password">
+                                            </div>
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        </div>
+                                        <!-- check -->
+                                        <div class="d-flex justify-content-between my-3">
+                                            <div class="form-check ">
+                                                <input
+                                                    class="form-check-input form-check-input-dark"
+                                                    type="checkbox" value=""
+                                                    id="flexCheckDefault" name="remember">
+                                                <label class="form-check-label"
+                                                    for="flexCheckDefault">{{ __('Remember me') }}</label>
+                                            </div>
+                                            @if (Route::has('password.request'))
+                                            <a href="{{ route('password.request') }}"
+                                                class=" nav-link text-decoration-underline text-reverse fw-semibold text-capitalize">{{ __('Forgot your password?') }}</a>
+                                            @endif
+                                        </div>
+                                        <!-- Button -->
+                                        <div class="form-group">
+                                            <x-primary-button class="btn btn-dark w-100">
+                                                {{ __('Log in') }}
+                                            </x-primary-button>
+                                        </div>
+                                        <div class="text-center mt-3 d-block d-sm-none">
+                                            <p class="text-muted mb-0">
+                                                Don't have an account
+                                                <a class="text-reverse text-decoration-underline" href="{{ route('register') }}">Register</a>
+                                            </p>
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,49 +11,50 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Core Style -->
-	    <link rel="stylesheet" href="/css/style.css">
-
-        <!-- Font Icons -->
-	    <link rel="stylesheet" href="/css/font-icons.css">
-
-        <!-- Plugins/Components CSS -->
-	    <link rel="stylesheet" href="/css/swiper.css">
-
-        <!-- Niche Demos -->
-        <link rel="stylesheet" href="/css/landing-2/landing-2.css">
-
-        <!-- Custom CSS -->
+        <link rel="stylesheet" href="/css/app/style.min.css">
         <link rel="stylesheet" href="/css/custom.css">
 
         <!-- Scripts -->
         @vite(['resources/js/app.js'])
     </head>
 
-    <body class="stretched">
-        <div id="wrapper">
-            <div class="position-absolute vh-100 w-100 top-0 start-0 overflow-hidden">
-                <img src="/images/guest/hero-bg.svg" alt="" class="hero-bg">
+    <body data-theme="theme-Greylight" class="svgstroke-a ">
+        <nav class="navbar navbar-expand-md position-fixed top-0 w-100 z-3 body-color ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="javascript:void(0)">
+                    <img src="/images/logo.svg" alt="SaasApps" class="h-44">
+                </a>
+                <div class="d-none d-sm-block">
+                    @if (Route::is('register'))
+                        <p class="text-muted">
+                            Already have an account
+                            <a class="text-reverse text-decoration-underline" href="{{ route('login') }}">Login</a>
+                        </p>
+                    @else
+                        <p class="text-muted">
+                            Don't have an account
+                            <a class="text-reverse text-decoration-underline" href="{{ route('register') }}">Register</a>
+                        </p>
+                    @endif
+               </div>
             </div>
-
-            @include('layouts.guest.header')
-
-            <section id="content" class="bg-transparent">
-                <div class="content-wrap overflow-visible pb-0">
-                    {{ $slot }}
+        </nav>
+        <main class="container-fluid px-0">
+            <!-- start: page menu link -->
+            <div class="content">
+                {{ $slot }}
+            </div>
+        </main>
+        <footer class="w-100 bg-body position-fixed bottom-0">
+            <div class="container-fluid">
+                <div class="d-flex align-items-center">
+                    <div class="p-0 m-0">
+                        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover text-reverse" href="javascript:void(0)">
+                            Copyrights &copy; {{ date('Y') }} All Rights Reserved by SaaSApps.
+                        </a>
+                    </div>
                 </div>
-            </section>
-            @include('layouts.guest.footer')
-        </div>
-
-        <!-- JavaScripts
-        ============================================= -->
-        <script src="/js/guest/plugins.min.js"></script>
-        <script src="/js/guest/functions.bundle.js"></script>
-
-        <!-- Parallax Script
-        ============================================= -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/skrollr/0.6.30/skrollr.min.js"></script>
-        <script>var s = !SEMICOLON.Mobile.any() && skrollr.init({ forceHeight: false });</script>
+            </div>
+         </footer>
     </body>
 </html>
